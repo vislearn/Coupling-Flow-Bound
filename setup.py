37a09d076a1a887082edd731ea5cfafec209951e
Felix Draxler <felix.draxler@iwr.uni-heidelberg.de>
2023-06-12 18:49:11 +0200

    pip setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Coupling-Flow-Bound",
+    version="1.0",
+    description="Code and experiments for the convergence rate bound for coupling-based normalizing flows on the non-Standardness",
+    url="https://github.com/vislearn/Coupling-Flow-Bound",
+    author="Felix Draxler",
+    author_email="felix.draxler@iwr.uni-heidelberg.de",
+    install_requires=[],
+    setup_requires=[],
+    packages=find_packages(),
+    ext_package="",
+)
