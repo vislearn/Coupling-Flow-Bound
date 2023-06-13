c52e2eb0e4953b859e301c247bceaf7bb2be5b14
Felix Draxler <felix.draxler@iwr.uni-heidelberg.de>
2023-06-13 16:56:59 +0200

    Export loss submodule

--- coupling_bound/__init__.py
+++ coupling_bound/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 import coupling_bound.orbit_projection as orbit_projection
 import coupling_bound.data as data
+import coupling_bound.loss as loss
 import coupling_bound.empiric as empiric
 import coupling_bound.vis as vis
 
